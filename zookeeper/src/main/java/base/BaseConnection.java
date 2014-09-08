6c669b339947fa310d5b4752407ff76d8e686d4f
Ethan <stillotherguy@gmail.com>
2014-09-08 14:46:17 +0800

    refactor

--- zookeeper/src/main/java/base/BaseConnection.java
+++ zookeeper/src/main/java/base/BaseConnection.java
@@ -1,4 +1,4 @@
-package example;
+package base;
 
 import org.apache.zookeeper.*;
 
@@ -8,7 +8,7 @@ import java.util.concurrent.CountDownLatch;
 /**
  * Created by Ethan on 14-9-6.
  */
-public class CreateGroup implements Watcher{
+public class BaseConnection implements Watcher{
     private static final int SESSION_TIMEOUT = 5000;
 
     private CountDownLatch connectedSignal = new CountDownLatch(1);
@@ -26,20 +26,20 @@ public class CreateGroup implements Watcher{
         connectedSignal.await();
     }
 
-    public void create(String groupName) throws KeeperException, InterruptedException {
+    /*public void create(String groupName) throws KeeperException, InterruptedException {
         String path = "/" + groupName;
         String createPath = zk.create(path,null, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
         System.out.println("Created " + createPath);
-    }
+    }*/
 
     public void close() throws InterruptedException {
         zk.close();
     }
 
-    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
-        CreateGroup createGroup = new CreateGroup();
+    /*public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
+        BaseConnection createGroup = new BaseConnection();
         createGroup.connect(args[0]);
         createGroup.create(args[1]);
         createGroup.close();
-    }
+    }*/
 }
